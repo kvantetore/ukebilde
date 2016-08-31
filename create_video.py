122e97915e6a8cef20df43070800acfedff2ba37
Tore Birkeland <tore@vostopia.com>
2016-08-31 17:58:50 +0200

    Added caching of source images when creating video

--- create_video.py
+++ create_video.py
@@ -3,11 +3,18 @@ import cv2
 import os
 from collections import namedtuple
 import exifread
+from functools32 import lru_cache
 
 out_folder = "./frames"
 
 FramePart = namedtuple("FramePart", "filename capture_date weight")
 Frame = namedtuple("Frame", "frame_index frame_parts")
+
+@lru_cache(maxsize=4)
+def read_image(filename, video_size):
+    print "reading", filename
+    img = cv2.imread(filename)
+    return cv2.resize(img, video_size)
 
 
 class Timeline():
@@ -50,12 +57,11 @@ class Timeline():
         return frames
 
     def read_image(self, filename):
-        print filename
-        img = cv2.imread(filename)
-        return cv2.resize(img, self.video_size)
+        return read_image(filename, self.video_size)
 
     def render_frame(self, frame):
         parts = frame.frame_parts
+
 
         #blend between frame parts
         source = self.read_image(parts[0].filename)
@@ -74,6 +80,8 @@ class Timeline():
             cv2.putText(source, "%imnd" % months, (int(source.shape[1]/20), int(source.shape[1]/15)), cv2.FONT_HERSHEY_SIMPLEX, 2, (200, 200, 200), 5)
 
         out_path = os.path.join(out_folder, "frame-{:0>5d}.jpg".format(frame.frame_index))
+        print "writing", out_path
+
         cv2.imwrite(out_path, source)
 
     def render(self):
@@ -81,7 +89,7 @@ class Timeline():
             self.render_frame(frame)
 
 
-def render_frames(filenames, capture_dates, delay=1.0, fade=0.2):
+def render_frames(filenames, capture_dates, delay=.3, fade=0.2):
     if not os.path.exists(out_folder):
         os.makedirs(out_folder)
     timeline = Timeline(filenames, capture_dates, delay, fade)
